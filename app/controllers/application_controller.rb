bfb6bb51c6ae33c5eb5d713a6823e37b0f8b56ef
m-kawahigashi <masa.15.30@icloud.com>
2019-05-22 19:27:10 +0900

    group coutroller model 作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!       #ログイン済ユーザーのみにアクセスを許可する処理。
+  before_action :configure_permitted_parameters, if: :devise_controller?   
+
+  protected
+
+  def configure_permitted_parameters                            #追加するパラメータの許可処理。
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])  #nameカラムの追加許可。
+  end
 end
