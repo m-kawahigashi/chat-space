145f1fa8afa4c9c45014c2bbbe3cab3fdfb7ee33
m-kawahigashi <masa.15.30@icloud.com>
2019-06-17 12:27:38 +0900

    抜けていた（）を記述修正した

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock "~> 3.11.0"
 set :application, "chat-space"
 set :repo_url,  "git@example.com:m-kawahigashi/chat-space.git"
 
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads'
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads'）
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
