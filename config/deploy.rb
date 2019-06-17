3bbdda19e43e9eee647ae00d22bbe5626d44b1cd
m-kawahigashi <masa.15.30@icloud.com>
2019-06-17 12:08:03 +0900

    deploy.rbを修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,12 +2,12 @@
 lock "~> 3.11.0"
 
 set :application, "chat-space"
-set :repo_url, "git@example.com:m-kawahigashi/chat-space.git"
+set :repo_url,  "git@example.com:m-kawahigashi/chat-space.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads'
 
 set :rbenv_type, :user
-set :rbenv_ruby, 'ruby 2.5.1'
+set :rbenv_ruby, '2.5.1'
 
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['ssh -i masanori0624.pem ec2-user@3.130.152.179']
